8c15cad622b7bc7764046cbc80af42fa083b30de
offtkp <parisoplop@gmail.com>
2023-10-27 17:29:24 +0300

    Fixup signedness

--- include/hydra/core.hxx
+++ include/hydra/core.hxx
@@ -27,8 +27,8 @@
 namespace hydra
 {
 
-    constexpr int32_t TOUCH_RELEASED = 0xFFFF'FFFF;
-    constexpr int32_t BAD_CHEAT = 0xFFFF'FFFF;
+    constexpr uint32_t TOUCH_RELEASED = 0xFFFF'FFFF;
+    constexpr uint32_t BAD_CHEAT = 0xFFFF'FFFF;
 
     /// Some things we want from type_traits for compile-time type checking, but we don't want to
     /// include the whole thing
